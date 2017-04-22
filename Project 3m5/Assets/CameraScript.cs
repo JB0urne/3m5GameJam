f9356c7e5a156d7f7c7343a8253264300706b393
3M5\reuss <lars.reuss@3m5.de>
2017-04-22 13:06:08 +0200

    Camera in Prefab

--- Project 3m5/Assets/CameraScript.cs
+++ Project 3m5/Assets/CameraScript.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
 
+    public GameObject playerPosition;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        this.followPlayer();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if (Input.GetKeyDown("space"))
+            print("space key was pressed");
+
+        this.followPlayer();
 	}
+
+    private void followPlayer()
+    {
+        Vector3 v = new Vector3(playerPosition.transform.position.x, playerPosition.transform.position.y, -10);
+        transform.position = v;
+    }
 }
