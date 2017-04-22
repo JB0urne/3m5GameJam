f7efd3d06e5215a624484cb59b74b5b66ab9d285
Steve Gotzel <steve@gotzel.com>
2017-04-22 12:22:19 +0200

    added tutorial

---
+++ Project 3m5/Assets/Scripts/UnitController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour {
+
+	public int myint;
+	private int myint2;
+	public GameObject myobject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
