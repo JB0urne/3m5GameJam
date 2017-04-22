ecd9aebd6ab00808cdeee0058fd5dc792f85a51b
Steve Gotzel <steve@gotzel.com>
2017-04-22 16:01:07 +0200

    healthaddon

---
+++ Project 3m5/Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public int health = 500;
+	public Slider healthbar;
+	// Use this for initialization
+	void Start () {
+		healthbar.value = health;	
+		healthbar.maxValue = health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (healthbar.value<=0) {
+			//gameover
+		} {
+			healthbar.value -= 1;
+		}; 
+	}
+
+	void ADDhealth (int amount) {
+		health += amount;
+	}
+
+	void SUBhealth (int amount) {
+		health -= amount;
+	}
+}
