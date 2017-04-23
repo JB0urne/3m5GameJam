8a2ec611c2e2cef420cc9421d9c6d8a1cda12503
Steve Gotzel <steve@gotzel.com>
2017-04-23 14:38:11 +0200

    GameController added score

--- Project 3m5/Assets/Scripts/GameController.cs
+++ Project 3m5/Assets/Scripts/GameController.cs
@@ -6,11 +6,22 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour {
 
 	public int health = 500;
+	public int ScorePerPickUp = 50;
+	public int ScorePerBlock = 10;
+	public int scorePerSecond = 5;
 	public Slider healthbar;
+	public Text scoreText;
+	public Text timeText;
+	private int score;
+	private float timer = 0.0f;
+	private int seconds;
+
+
 	// Use this for initialization
 	void Start () {
 		healthbar.value = health;	
 		healthbar.maxValue = health;
+		score = 0;
 	}
 	
 	// Update is called once per frame
@@ -21,6 +32,13 @@ public class GameController : MonoBehaviour {
 			//gameover
 		}
 		healthbar.value = health;
+
+		//update time
+		timer += Time.deltaTime;
+		seconds = (int) timer;
+		timeText.text = seconds.ToString();
+
+		SetScoreText ();
 	}
 
 	public void ADDhealth (int amount) {
@@ -29,5 +47,21 @@ public class GameController : MonoBehaviour {
 
 	public void SUBhealth (int amount) {
 		health -= amount;
+	}
+
+	public void ADDscorePickUp () {
+		score += ScorePerPickUp;
+	}
+
+	public void ADDscoreBlocks () {
+		score += ScorePerBlock;
+	}
+
+	public void SUBscore (int amount) {
+		score -= amount;
+	}
+
+	public void SetScoreText () {
+		scoreText.text = (seconds*scorePerSecond + score).ToString();
 	}
 }
